36f41ab9505c4c403de02c6ba4ecc34180e5a09d
Mei <meixintongzha@gmail.com>
2017-12-02 13:50:26 -0500

    camera worked

--- app/src/main/java/com/trash/recyclablerecognition/MainActivity.java
+++ app/src/main/java/com/trash/recyclablerecognition/MainActivity.java
@@ -1,17 +1,66 @@
 package com.trash.recyclablerecognition;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 /**
  * Created by kyle on 12/2/17.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.image_view);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(MainActivity.this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+            }
+        });
+    }
+
+    private File getFile()
+    {
+        File folder = new File("sdcard/camera_app");
+
+        if(!folder.exists())
+        {
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder,"cam_image.jpg");
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
 }
