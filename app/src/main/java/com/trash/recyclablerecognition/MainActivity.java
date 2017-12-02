d858e8cb485c89227a9482f936e3d0135c26a699
Kyle Corry <kylecorry31@gmail.com>
2017-12-02 14:30:44 -0500

    Fix permission issue

--- app/src/main/java/com/trash/recyclablerecognition/MainActivity.java
+++ app/src/main/java/com/trash/recyclablerecognition/MainActivity.java
@@ -1,13 +1,17 @@
 package com.trash.recyclablerecognition;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -29,6 +33,14 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        0);
+        }
         button = (Button) findViewById(R.id.button);
         imageView = (ImageView)findViewById(R.id.image_view);
         button.setOnClickListener(new View.OnClickListener(){
